73bff617264b33e2569393cc23807e46bc62e567
Sarthak Sahu <sarsahu@microsoft.com>
2020-01-18 11:29:10 +0530

    Add styles to signup view

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Button, Input } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
-      <Text style={{ fontSize: 48 }}> SignupScreen </Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SignupScreen;
