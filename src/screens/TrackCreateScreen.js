b4bf630db7fb12b774c016f1744fbe130af2c653
Sarthak Sahu <vishalsahu5@gmail.com>
2020-02-10 14:58:22 +0530

    Automatic disable recording on screen navigate

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,16 +2,18 @@ import "../utils/_mockLocation";
 import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import { SafeAreaView } from "react-navigation";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
 import useLocation from "../hooks/useLocation";
 
 import { Context as LocationContext } from "../context/LocationContext";
 
 import Map from "../components/Map";
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+  const [err] = useLocation(isFocused, addLocation);
+
+  console.log(isFocused);
 
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
@@ -24,4 +26,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
